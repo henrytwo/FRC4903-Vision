2000f5d3c7482f30ab8bdd33fe2d25411c9299fb
Henry T <henryt.inc@gmail.com>
2018-11-09 21:49:12 -0500

    Started multiprocessing

--- UTarget.py
+++ UTarget.py
@@ -1,120 +1,162 @@
 import cv2
 import numpy as np
+import multiprocessing
+from networktables import NetworkTables
 
-# Selecting camera
-cap = cv2.VideoCapture(0)
+def locked():
+    print('Locked on target')
 
-# Set frame size
-cap.set(3, 1024)
-cap.set(4, 615)
+def unlocked():
+    print('Target not locked')
 
-# FOV of the camera
-FOV = 125.718
+def idle():
+    print('System idle')
 
-# Makes the window
-cv2.namedWindow('image')
+def error():
+    print('Something went wrong')
 
-# Size of the image
-HEIGHT, WIDTH, IDK = cap.read()[1].shape
+def initializing():
+    print('Initializing')
 
-# DO this forever
-while True:
+def nt_read():
+    pass
 
-    # Get frame
-    _, frame = cap.read()
+def nt_write():
+    pass
 
-    # Convert to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+def scan(sq):
+    # DO this forever
+    while True:
 
-    # Set upper and lower boundary
-    upper_red = np.array([255, 255, 255])
-    lower_red = np.array([0, 0, 126])
+        # Kills scan process
+        try:
+            if not instruction.empty():
+                instruction = sq.get()
 
-    # Create mask
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+                if instruction == 'stop':
+                    return True
+        except:
+            pass
 
-    im2, cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        # Get frame
+        _, frame = cap.read()
 
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:2]
+        # Convert to HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    rects = []
-    points = []
+        # Set upper and lower boundary
+        upper_thresh = np.array([255, 255, 255])
+        lower_thresh = np.array([0, 0, 126])
 
-    for c in cnts:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        x, y, w, h = cv2.boundingRect(approx)
+        # Create mask
+        mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
+        res = cv2.bitwise_and(frame, frame, mask=mask)
 
-        if h >= 10 and w >= 10:
-            # if height is enough
-            # create rectangle for bounding
-            rect = (x, y, w, h)
-            rects.append(rect)
+        im2, cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-            #w * h > points[2] * points[3] and
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:2]
 
-            if not points or (abs(len(approx) - 8) < abs(points[4] - 8)):
-                points = [x + w // 2, y + h // 2, w, h, len(approx)]
+        rects = []
+        points = []
 
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 1)
+        for c in cnts:
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+            x, y, w, h = cv2.boundingRect(approx)
 
-                prev = ()
+            if h >= 10 and w >= 10:
+                # if height is enough
+                # create rectangle for bounding
+                rect = (x, y, w, h)
+                rects.append(rect)
 
-                for i in approx:
-                   #print(i[0][0])
+                #w * h > points[2] * points[3] and
 
-                   if prev:
-                       cv2.line(frame, prev, (i[0][0], i[0][1]), (255, 255, 0), 1)
+                if not points or (abs(len(approx) - 8) < abs(points[4] - 8)):
+                    points = [x + w // 2, y + h // 2, w, h, len(approx)]
 
-                   prev = (i[0][0], i[0][1])
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 1)
 
-                   cv2.line(frame, (i[0][0] - 10, i[0][1]), (i[0][0] + 10, i[0][1]), (255, 0, 255), 1)
-                   cv2.line(frame, (i[0][0], i[0][1] - 10), (i[0][0], i[0][1] + 10), (255, 0, 255), 1)
+                    prev = ()
+
+                    for i in approx:
+                       #print(i[0][0])
+
+                       if prev:
+                           cv2.line(frame, prev, (i[0][0], i[0][1]), (255, 255, 0), 1)
+
+                       prev = (i[0][0], i[0][1])
+
+                       cv2.line(frame, (i[0][0] - 10, i[0][1]), (i[0][0] + 10, i[0][1]), (255, 0, 255), 1)
+                       cv2.line(frame, (i[0][0], i[0][1] - 10), (i[0][0], i[0][1] + 10), (255, 0, 255), 1)
 
 
+                else:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+                cv2.putText(frame, 'I think this has %i sides' % len(approx), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+                            (0, 255, 0), 1, cv2.LINE_AA)
+
+        if points and points[4] > 4:
+            mx = points[0]
+            my = points[1]
+
+            angle = FOV * (mx / WIDTH) - FOV / 2
+
+            if angle > 0:
+                move = 'move left! <='
+            elif angle < 0:
+                move = 'move right! =>'
             else:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                move = 'don\'t move! </>'
 
-            cv2.putText(frame, 'I think this has %i sides' % len(approx), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                        (0, 255, 0), 1, cv2.LINE_AA)
+            #cv2.circle(frame, (mx, my), 20, (0, 0, 255), 1)
 
-    if points and points[4] > 4:
-        mx = points[0]
-        my = points[1]
+            cv2.line(frame, (mx - 10, my), (mx + 10, my), (0, 0, 255), 1)
+            cv2.line(frame,  (mx, my - 10), (mx, my + 10), (0, 0, 255), 1)
 
-        angle = FOV * (mx / WIDTH) - FOV / 2
-
-        if angle > 0:
-            move = 'move left! <='
-        elif angle < 0:
-            move = 'move right! =>'
+            cv2.putText(frame,
+                        'Deviation: %i | Angle to center: %2.2f degrees | (This means you %s)' % (points[4] - 8, angle, move),
+                        (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1, cv2.LINE_AA)
+            print(angle, mx - WIDTH // 2)
         else:
-            move = 'don\'t move! </>'
+            cv2.putText(frame, 'Target not found', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1, cv2.LINE_AA)
 
-        #cv2.circle(frame, (mx, my), 20, (0, 0, 255), 1)
+        cv2.line(frame, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), (0, 0, 255), 1)
 
-        cv2.line(frame, (mx - 10, my), (mx + 10, my), (0, 0, 255), 1)
-        cv2.line(frame,  (mx, my - 10), (mx, my + 10), (0, 0, 255), 1)
+        res = cv2.bitwise_and(frame, frame, mask=mask)
 
-        cv2.putText(frame,
-                    'Deviation: %i | Angle to center: %2.2f degrees | (This means you %s)' % (points[4] - 8, angle, move),
-                    (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1, cv2.LINE_AA)
-        print(angle, mx - WIDTH // 2)
-    else:
-        cv2.putText(frame, 'Target not found', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        cv2.imshow('image', mask)
+        cv2.imshow('res', res)
 
-    cv2.line(frame, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), (0, 0, 255), 1)
+if __name__ == '__main__':
 
-    res = cv2.bitwise_and(frame, frame, mask=mask)
+    try:
+        # Selecting camera
+        cap = cv2.VideoCapture(0)
 
-    cv2.imshow('frame', frame)
-    cv2.imshow('image', mask)
-    cv2.imshow('res', res)
+        # Set frame size
+        cap.set(3, 1024)
+        cap.set(4, 615)
 
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+        # FOV of the camera
+        FOV = 125.718
 
-cv2.destroyAllWindows()
-cap.release()
+        # Makes the window
+        cv2.namedWindow('image')
+
+        # Size of the image
+        HEIGHT, WIDTH, _ = cap.read()[1].shape
+
+        NetworkTables.initialize(server='10.49.03.2')
+
+        scan_queue = multiprocessing.Queue()
+
+        s = multiprocessing.Process(target=scan, args=(scan_queue)).start()
+
+    except:
+        error()
+
+#cv2.destroyAllWindows()
+#cap.release()
