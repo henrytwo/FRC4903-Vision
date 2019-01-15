0853964b13e7458190a45418832a670779178346
Henry T <henryt.inc@gmail.com>
2019-01-15 17:35:26 -0500

    2019 vision

--- UTarget.py
+++ UTarget.py
@@ -18,20 +18,6 @@ else:
 
 camID = 2
 
-
-def changeListener(key, value, isNew):
-    if key == '/Vision/enabled':
-        print('Scan mode:', value)
-        halt_queue.put('GO' if value else 'STOP')
-
-    elif key == '/Vision/time':
-        offset = time.time() - value
-        print('TIME OFFSET', offset)
-        table.putNumber('offset', offset)
-
-        # network_ready()
-
-
 if __name__ == '__main__':
 
     try:
@@ -48,20 +34,10 @@ if __name__ == '__main__':
         print('Cam ID %i succeeded' % camID)
 
         # Set frame size
-        cap.set(3, 1024)
-        cap.set(4, 615)
-
-        NetworkTables.initialize(server='localhost')
-        NetworkTables.addEntryListener(changeListener)
-        table = NetworkTables.getTable("Vision")
-
-        table.putNumber('getTime', 1)
-
-        logging.basicConfig(level=logging.DEBUG)
+        #cap.set(3, 1024)
+        #cap.set(4, 615)
 
         offset = 0
-
-        halt_queue = multiprocessing.Queue()
 
         # Set upper and lower boundary
         upper_thresh = np.array([255, 255, 255])
@@ -73,6 +49,9 @@ if __name__ == '__main__':
         # Size of the image
         HEIGHT, WIDTH, _ = cap.read()[1].shape
 
+        Y_DEVIATION = 100
+        X_DEVIATION = 200
+
         halted = True
         target_locked = False
 
@@ -83,22 +62,9 @@ if __name__ == '__main__':
 
             prev_time = time.time()
 
-            if halted:
-
-                # disabled()
-
-                h = halt_queue.get()  # STOP HERE
-
-                if h == 'GO':
-                    # unlocked()
-                    halted = False
-
-            elif not halt_queue.empty():
-                if halt_queue.get() == 'STOP':
-                    halted = True
-
             # Get frame
-            _, frame = cap.read()
+            #_, frame = cap.read()
+            frame = cv2.imread('manydark.png', 1)
 
             # Convert to HSV
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -107,16 +73,28 @@ if __name__ == '__main__':
             mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
 
             im2, cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:2]
+            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
             rects = []
             points = []
-            max_area = -1
+            #max_area = -1
+
+            left = []
+            right = []
+            group = []
 
             for c in cnts:
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.02 * peri, True)
                 x, y, w, h = cv2.boundingRect(approx)
+
+                approx = list(approx)
+
+                approx.append(approx[0])
+
+                longest = -1
+                longest_coords = []
+
 
                 if h >= 20 and w >= 20:
                     # if height is enough
@@ -124,35 +102,87 @@ if __name__ == '__main__':
                     rect = (x, y, w, h)
                     rects.append(rect)
 
-                    if (not points or (abs(len(approx) - 8) < abs(points[4] - 8))) and w * h > max_area:
-                        points = [x + w // 2, y + h // 2, w, h, len(approx)]
-                        max_area = w * h
+                    if abs(len(approx) - 4) <= 2: #(not points or (abs(len(approx) - 4) < abs(points[4] - 4))) and w * h > max_area:
+                        points = [x + w // 2, y + h // 2, x, y, w, h, len(approx)]
+                        #max_area = w * h
 
                         if not HEADLESS:
-                            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 1)
+                            #cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 1)
 
                             prev = ()
 
+                            count = 0
+
                             for i in approx:
                                 if prev:
-                                    cv2.line(frame, prev, (i[0][0], i[0][1]), (255, 255, 0), 1)
+
+                                    if math.hypot(i[0][0] - prev[0], i[0][1] - prev[1]) > longest:
+                                        longest = math.hypot(i[0][0] - prev[0], i[0][1] - prev[1])
+                                        longest_coords = [prev, (i[0][0], i[0][1])]
+
+                                    cv2.line(frame, prev, (i[0][0], i[0][1]), (0, 0, 255), 1)
 
                                 prev = (i[0][0], i[0][1])
+
+                                count += 1
+
+                                cv2.putText(frame,
+                                            '%i' % (count),
+                                            (i[0][0], i[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+                                            (255, 255, 255), 1, cv2.LINE_AA)
 
                                 cv2.line(frame, (i[0][0] - 10, i[0][1]), (i[0][0] + 10, i[0][1]), (255, 0, 255), 1)
                                 cv2.line(frame, (i[0][0], i[0][1] - 10), (i[0][0], i[0][1] + 10), (255, 0, 255), 1)
 
-                            # cv2.drawContours(frame, approx, 0, (255, 255, 0), 2)
-                    else:
+                            if longest_coords:
+                                cv2.line(frame, longest_coords[0], longest_coords[1], (0, 255, 255), 2)
 
-                        if not HEADLESS:
-                            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                                if (longest_coords[0][1] - longest_coords[1][1]) / (longest_coords[0][0] - longest_coords[1][0]) >= 0:
 
-                    if not HEADLESS:
-                        cv2.putText(frame, 'I think this has %i sides and dis dis big: %i' % (len(approx), w * h),
-                                    (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                                    (0, 255, 0), 1, cv2.LINE_AA)
+                                    cv2.putText(frame, 'RIGHT',
+                                                longest_coords[0], cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+                                                (255, 255, 255), 1, cv2.LINE_AA)
 
+                                    right.append(points)
+
+                                else:
+
+                                    cv2.putText(frame, 'LEFT',
+                                                longest_coords[0], cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+                                                (255, 255, 255), 1, cv2.LINE_AA)
+
+                                    left.append(points)
+
+            left.sort()
+            right.sort()
+
+            for l in range(len(left)):
+
+                for r in range(len(right)):
+
+                    if abs(left[l][0] - right[r][0]) <= X_DEVIATION and abs(left[l][1] - right[r][1]) <= Y_DEVIATION and left[l][0] < right[r][0]:
+
+                        left_piece = left[l][:]
+                        right_piece = right[r][:]
+
+                        del right[r]
+
+                        break
+
+                else:
+                    continue
+
+                cv2.circle(frame, (left_piece[0], left_piece[1]), 10, (255, 0, 0), 1)
+                cv2.circle(frame, (right_piece[0], right_piece[1]), 10, (255, 0, 0), 1)
+
+                cv2.rectangle(frame, (left_piece[2], left_piece[3]), (left_piece[2] + left_piece[4], left_piece[3] + left_piece[5]), (255, 0, 0), 1)
+                cv2.rectangle(frame, (right_piece[2], right_piece[3]), (right_piece[2] + right_piece[4], right_piece[3] + right_piece[5]), (0, 255, 0), 1)
+
+                cv2.rectangle(frame, (left_piece[2], min(left_piece[3], right_piece[3])), (right_piece[2] + right_piece[4], max(left_piece[3] + left_piece[5], right_piece[3] + right_piece[5])), (255, 255, 0), 1)
+
+            #print('left', left, '\nright', right)
+
+            """
             if points and 10 > points[4] > 4:
                 mx = points[0]
                 my = points[1]
@@ -177,13 +207,13 @@ if __name__ == '__main__':
                                     points[4] - 8, angle, move),
                                 (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1, cv2.LINE_AA)
 
-                table.putNumber('heading', angle)
-                table.putNumber('deviation', points[4] - 8)
-                table.putNumber('lastUpdated', time.time() + offset)
+                #table.putNumber('heading', angle)
+                #table.putNumber('deviation', points[4] - 8)
+                #table.putNumber('lastUpdated', time.time() + offset)
 
-                if not target_locked:
-                    target_locked = True
-                    table.putNumber('locked', 1)
+                #if not target_locked:
+                #    target_locked = True
+                #    table.putNumber('locked', 1)
 
                     # locked()
             else:
@@ -201,6 +231,7 @@ if __name__ == '__main__':
                 if not HEADLESS:
                     cv2.putText(frame, 'Target not found', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1,
                                 cv2.LINE_AA)
+            """
 
             if not HEADLESS:
                 cv2.line(frame, (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), (0, 0, 255), 1)
