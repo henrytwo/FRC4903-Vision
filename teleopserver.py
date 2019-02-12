1649411c4e610f2b66d58f4b66973e9287b22b07
Henry T <henryt.inc@gmail.com>
2019-02-11 21:01:28 -0500

    jQuery + Camera flip button

--- teleopserver.py
+++ teleopserver.py
@@ -13,6 +13,7 @@
 #sudo apt-get install libqt4-test
 
 import cv2
+import glob
 from PIL import Image
 from http.server import BaseHTTPRequestHandler,HTTPServer
 from socketserver import ThreadingMixIn
@@ -30,57 +31,21 @@ else:
 
 PORT = 8080
 
-CAM_DRIVE = 'USB_2.0_Camera' #'/dev/v4l/by-id/usb-HD_Camera_Manufacturer_USB_2.0_Camera-video-index0'
-CAM_MECH = 'Logitech_Webcam_C930e' #'/dev/v4l/by-id/usb-046d_Logitech_Webcam_C930e_74B595EE-video-index0'
+CAM_DRIVE = '/dev/v4l/by-id/usb-Guillemot_Corporation_USB_Camera-video-index0' #0 #'/dev/v4l/by-id/usb-HD_Camera_Manufacturer_USB_2.0_Camera-video-index0'
+CAM_MECH = '/dev/v4l/by-id/usb-SunplusIT_INC._Integrated_Camera-video-index1' #1 #'/dev/v4l/by-id/usb-046d_Logitech_Webcam_C930e_74B595EE-video-index0'
 
-devices = subprocess.Popen(['ls', '/dev'], stdout=subprocess.PIPE).communicate()[0].split(b'\n')
-model_to_id = {}
-
-def get_model(devID):
-
-	try:
-		data = subprocess.Popen(('udevadm info --query=all /dev/video' + str(devID)).split(), stdout=subprocess.PIPE).communicate()[0].split(b'\n')
-
-		for d in data:
-			if b'ID_MODEL' in d:
-
-				return d.split(b'=')[1]
-	except:
-		traceback.print_exc()
-
-	return -1
-
-for d in devices:
-	if b'video' in d:
-
-		d = int(d.strip(b'video'))
-
-		cam_model = get_model(d)
-
-		#print('GG', d, cam_model)
-
-		works = False
-
-		try:
-			c = cv2.VideoCapture(d)
-			works = c.read()[1]
-		except:
-			pass
-
-		if works:
-			model_to_id[cam_model] = d
-
-
-print(model_to_id)
-
-
+vert = ['primaryFrame', 'mechFrame']
 
 class CamHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 
 		print(self.path)
 
-		if self.path.endswith('.mjpg'):
+		if self.path.endswith('flip'):
+			vert.append(vert[0])
+			del vert[0]
+
+		elif self.path.endswith('.mjpg'):
 
 			frame_name = self.path[1:].split('.mjpg')[0]
 
@@ -92,6 +57,9 @@ class CamHandler(BaseHTTPRequestHandler):
 				while True:
 					try:
 						img = frames[frame_name]['frame']()
+
+						if vert[0] == frame_name:
+							img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
 						imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -113,14 +81,26 @@ class CamHandler(BaseHTTPRequestHandler):
 					except KeyboardInterrupt:
 						break
 
-			else:
-				print('no u')
 
 			self.send_response(404)
 			self.send_header('Content-type', 'text/html')
 			self.end_headers()
 
 			return
+
+		elif 'public' + self.path in glob.glob('public/*'):
+
+			with open('public' + self.path, 'r') as file:
+
+
+
+				self.send_response(200)
+				self.send_header('Content-type', 'text/js')
+				self.end_headers()
+				self.wfile.write(file.read().encode())
+
+				return
+
 		else:
 			with open('index.html', 'r') as main_page:
 
@@ -130,7 +110,7 @@ class CamHandler(BaseHTTPRequestHandler):
 					cam_page_template = cam_page.read()
 
 					for camName in frames:
-						cams += cam_page_template.format(MACHINENAME=camName, PORT=PORT, HOST=HOST, HUMANNAME=frames[camName]['humanName'])
+						cams += cam_page_template.format(MACHINENAME=camName, PORT=PORT, HOST=HOST)
 
 					page = main_page.read().format(CAMS=cams).encode()
 
@@ -145,13 +125,11 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 	pass
 
 class TeleopCam:
-	def __init__(self, id, w, h, enforced, rotate):
+	def __init__(self, id, w, h, enforced):
 		self.id = id
 		self.enforced = enforced
 		self.w = w
 		self.h = h
-
-		self.rotate = rotate
 
 		self.capture = cv2.VideoCapture(self.id)
 
@@ -164,62 +142,41 @@ class TeleopCam:
 
 			self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, enforced[0])
 			self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, enforced[1])
+
 	def getFrame(self):
 		rc, img = self.capture.read()
 
 		if self.enforced:
 			img = cv2.resize(img, (self.w, self.h))
 
-		if self.rotate:
-			img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-
-		cv2.putText(img, str(datetime.datetime.now()),
-					(10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
-					(255, 255, 255), 1, cv2.LINE_AA)
+		#cv2.putText(img, str(datetime.datetime.now()),
+		#			(10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
+		#			(255, 255, 255), 1, cv2.LINE_AA)
 
 		return img
 
-def main():
-	global frames
+primaryCam = TeleopCam(CAM_DRIVE, int(683 * 0.30), int(384 * 0.30), (int(683 * 0.65), int(384 * 0.65)))
+mechCam = TeleopCam(CAM_MECH, int(683 * 0.30), int(384 * 0.30), (int(683 * 0.65), int(384 * 0.65)))
 
-	"""
-	teleopBoi = TeleopCam(CAM_DRIVE, int(683 * 0.30), int(384 * 0.30), (int(683 * 0.65), int(384 * 0.65)), False)
-	otherBoi = TeleopCam(CAM_MECH, int(683 * 0.30), int(384 * 0.30), (int(683 * 0.65), int(384 * 0.65)), True)
+if __name__ == '__main__':
 
 	frames = {
-		'teleopFrame':  {
-			'frame': teleopBoi.getFrame,
+		'primaryFrame': {
+			'frame': primaryCam.getFrame,
 			'humanName': 'Teleop Raw Feed'
 		},
-		'slideFrame': {
-			'frame': otherBoi.getFrame,
+		'mechFrame': {
+			'frame': mechCam.getFrame,
 			'humanName': 'Other stuff Feed'
 		}
 	}
-	"""
 
-	""",
-			'autoFrame': {
-				'frame': at.get_frame,
-				'humanName': 'CV Raw Feed'
-			},
-			'autoMask': {
-				'frame': at.get_mask,
-				'humanName': 'CV Mask'
-			},
-			'autoRes':  {
-				'frame': at.get_res,
-				'humanName': 'CV Overlay'
-			}"""
 
 	try:
 		server = ThreadedHTTPServer((HOST, PORT), CamHandler)
 		print("Server started @ (%s:%i)" % (HOST, PORT))
 		server.serve_forever()
 	except KeyboardInterrupt:
-		#capture.release()
-		#capture.release()
+		# capture.release()
+		# capture.release()
 		server.socket.close()
-
-if __name__ == '__main__':
-	main()
